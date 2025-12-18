7fb45d17504d6d3df86190c75b50f6996159d219
Likithapamuri2006 <likithapamuri2006@gmail.com>
2025-12-18 17:59:25 +0530

    Add files via upload

---
+++ Examplemethod4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Demo{
+   int sum,c;
+   int add(int a,int b){
+    c=a+b;
+    System.out.println(c);
+    return c;
+   }
+}
+public class Examplemethod4 {
+   public static void main(String[] args) {
+    Demo d=new Demo();
+    d.add(10,20);
+   } 
+} 
