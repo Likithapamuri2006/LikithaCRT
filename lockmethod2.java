deae2d4af48d5156b6e6705b27d3807418c2ed53
Likithapamuri2006 <likithapamuri2006@gmail.com>
2025-12-19 11:29:49 +0530

    Add files via upload

---
+++ lockmethod2.java
@@ -0,0 +1,35 @@
+class Printer implements Runnable{
+    public void run(){
+        String name=Thread.currentThread().getName();
+        System.out.println(name + " printing started");
+        for(int i=1;i<=5;i++){
+            synchronized(this){
+            System.out.println(name + " is printing");
+            }
+            try{
+                Thread.sleep(2000);
+            }
+            catch(Exception e){
+                System.out.println("some problem occured");
+            }
+        }
+        System.out.println(name+"complete the printing");
+    }
+}
+
+public class lockmethod2 {
+    public static void main(String[] args) {
+        Printer p=new Printer();
+        Thread t1=new Thread(p);
+        Thread t2=new Thread(p);
+        Thread t3=new Thread(p);
+
+        t1.setName("likitha");
+        t2.setName("manasa");
+        t3.setName("akhila");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
